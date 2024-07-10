e33e3a96d800858c86e425868c9127ea941e3e33
AbuBakar <abubakarfarooq13@gmail.com>
2024-07-10 20:21:06 +0500

    oasisimage

---
+++ assets/kncLogo-ae512edf.js
@@ -0,0 +1,1 @@
+import{j as t,ge as x,v as a,ii as g}from"./index-20b3a7a2.js";function p({size:o}){return t.jsx("img",{src:`${x(g,a.MAINNET)}`,alt:"knc-logo",width:o?`${o}px`:"24px",height:o?`${o}px`:"24px"})}export{p as K};
