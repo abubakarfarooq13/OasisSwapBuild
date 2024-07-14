6b84da376c057ce02ba68858c7e00ef65c3f442b
AbuBakar <abubakarfarooq13@gmail.com>
2024-07-14 17:12:26 +0500

    tokens-design

--- assets/formatCurrencyAmount-68c4160e.js
+++
@@ -1,1 +0,0 @@
-import{ag as e,dT as n}from"./index-bd92f56a.js";function f(r,i){return r?e.equal(r.quotient,e.BigInt(0))?"0":r.divide(r.decimalScale).lessThan(new n(1,1e5))?"<0.00001":r.toSignificant(i):"-"}export{f};
---
+++ assets/kncLogo-ce706cfc.js
@@ -0,0 +1,1 @@
+import{j as t,gc as x,v as a,ie as g}from"./index-3b83b03c.js";function p({size:o}){return t.jsx("img",{src:`${x(g,a.MAINNET)}`,alt:"knc-logo",width:o?`${o}px`:"24px",height:o?`${o}px`:"24px"})}export{p as K};
